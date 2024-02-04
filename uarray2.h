028601e32fd98a5e670172f93158608fa2b9a6a1
ljhxksy <132593990+ljhxksy@users.noreply.github.com>
2024-02-04 14:25:51 -0500

    init bit2.h and uarray2.h

---
+++ uarray2.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "uarray.h"
+
+#ifndef UARRAY2
+#define UARRAY2
+
+#define T UArray2_T
+typedef struct T *T;
+struct T {
+    int width;
+    int height;
+    char *elems; //WHAT IS THIS DOING WHY IS IT A CHAR POINTER
+};
+
+extern T UArray2_new (int width, int height, int size);
+extern void UArray2_free(T *uarray2);
+extern int UArray2_width(T uarray2);
+extern int UArray2_height(T uarray2);
+extern int UArray2_size (T uarray2);
+void *UArray2_at(T uarray2, int width, int height);
+extern void UArray2_map_row_major (T uarray2, void apply(int width, 
+                                    int height, UArray2_T uarray2, void *p1, void *p2), void *cl);
+extern void UArray2_map_col_major (T uarray2, void apply(int width, 
+                                    int height, UArray2_T uarray2, void *p1, void *p2), void *cl);
+
+#undef T
+#endif
